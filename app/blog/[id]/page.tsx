9796615c1489e3839a150f8526f133932cb4cafd
Yurii Kozlov <yurka22870@gmail.com>
2023-05-25 16:57:41 +0300

    1

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,19 +1,23 @@
 import { Metadata } from "next";
+import { Post } from "types/Post";
 
-async function getData(id: string) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}`,
-    {
-      next: {
-        revalidate: 60,
-      },
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+async function getPostData(id: string): Promise<Post> {
+  const response = await fetch(`${baseURL}/posts/${id}`, {
+    next: {
+      revalidate: 60
     }
-  );
+  });
+
+  if (!response.ok) {
+    throw new Error('Unable to fetch posts!');
+  }
 
   return response.json();
 }
 
-type Props = {
+type PostProps = {
   params: {
     id: string;
   };
@@ -21,16 +25,16 @@ type Props = {
 
 export async function generateMetadata({
   params: { id },
-}: Props): Promise<Metadata> {
-  const post = await getData(id);
+}: PostProps): Promise<Metadata> {
+  const post = await getPostData(id);
 
   return {
     title: post.title,
   };
 }
 
-export default async function Post({ params: { id } }: Props) {
-  const post = await getData(id);
+export default async function Post({ params: { id } }: PostProps) {
+  const post = await getPostData(id);
 
   return (
     <>
