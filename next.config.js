ccb1fc5ae7d7579a4d1716e9124f7a073f373286
Yurii Kozlov <yurka22870@gmail.com>
2023-05-25 17:04:57 +0300

    2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  sassOptions: {
+    additionalData: `@import "styles/utils/variables.scss"; @import "styles/utils/mixins.scss";
+    @import "styles/utils/placeholders.scss";`,
+ }
+}
