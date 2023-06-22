d14079f6d9179cbba31beed22c617c4ab97c2996
Yurii Kozlov <yurka22870@gmail.com>
2023-06-22 16:25:31 +0300

    deployment fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,12 +6,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2022-11-15',
 });
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 export async function POST(req: NextRequest, res: NextResponse): Promise<Response | NextResponse> {
   const headersList = headers();
 
